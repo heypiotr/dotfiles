eb37a5a8605cf1fa689425cc749addceaa8551be
Piotr Krawiec <piotr@krawiec.me>
2015-05-25 19:45:24 +0000

    No need to version-control the .vim dir anymore

--- install.sh
+++ install.sh
@@ -8,8 +8,6 @@ ln -sfvn ${BASEDIR}/bash_profile ~/.bash_profile
 ln -sfvn ${BASEDIR}/inputrc ~/.inputrc
 
 # vim
-[[ -d ~/.vim ]] && rm -rf ~/.vim
-ln -sfvn ${BASEDIR}/vim ~/.vim
 ln -sfvn ${BASEDIR}/vimrc ~/.vimrc
 [[ ! -f ~/.vim/autoload/plug.vim ]] && \
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
